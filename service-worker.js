b7eacccc06f836f5e4ff5c381da3b859a3470bd2
Alexander Mut <mutalex@gmail.com>
2025-10-01 13:32:51 +0200

    Refine PWA manifest and worker lifecycle

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@ const ASSETS = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
   );
 });
 
@@ -19,7 +19,7 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
       keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
+    )).then(() => self.clients.claim())
   );
 });
 
